ae5bce662a1c744e9303e99cf6b9fbbb1f3b0195
brunaanjos <brunaanjosdaveiga@gmail.com>
2024-07-26 16:56:05 -0300

    criação dos layouts

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,51 @@
-import { createRouter, createWebHistory } from 'vue-router';
-import HomeView from '../views/HomeView.vue';
-import LayoutFull from '@/layouts/LayoutFull.vue';
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+import CategoriaView from "../views/CategoriaView.vue";
+import AcessorioView from "../views/AcessorioView.vue";
+import CorView from "../views/CorView.vue";
+import MarcaView from "../views/MarcaView.vue";
+
+import LoginView from "../views/LoginView.vue";
+import LogoutView from "../views/LogoutView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: '',
-      component: LayoutFull,
-      children: [
-        {
-          path: '',
-          name: 'Home',
-          component: HomeView,
-        },
-      ],
+      path: "/",
+      name: "home",
+      component: HomeView,
     },
+    {
+      path: "/categorias",
+      name: "categorias",
+      component: CategoriaView,
+    },
+    {
+      path: "/marcas",
+      name: "marcas",
+      component: MarcaView,
+    },
+    {
+      path: "/cores",
+      name: "cores",
+      component: CorView,
+    },
+    {
+      path: "/acessorios",
+      name: "acessorios",
+      component: AcessorioView,
+    },
+    {
+      path: "/login",
+      name: "login",
+      component: LoginView,
+    },
+    {
+      path: "/logout",
+      name: "logout",
+      component: LogoutView,
+    }
   ],
 });
 
